914e465b561b7dce663a45fade1361bdacf88bfc
lion772 <williamlopes64@gmail.com>
2023-05-31 16:18:01 +0200

    replace a tag with navlink in EventsNavigation

--- new-router-app/frontend/src/components/EventsNavigation.js
+++ new-router-app/frontend/src/components/EventsNavigation.js
@@ -1,20 +1,36 @@
-import classes from './EventsNavigation.module.css';
+import classes from "./EventsNavigation.module.css";
+import { NavLink } from "react-router-dom";
 
 function EventsNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a href="/events">All Events</a>
-          </li>
-          <li>
-            <a href="/events/new">New Event</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li>
+                        <NavLink
+                            to=""
+                            className={({ isActive }) =>
+                                isActive && classes.active
+                            }
+                            end
+                        >
+                            All Events
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="new"
+                            className={({ isActive }) =>
+                                isActive && classes.active
+                            }
+                        >
+                            New Event
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default EventsNavigation;
