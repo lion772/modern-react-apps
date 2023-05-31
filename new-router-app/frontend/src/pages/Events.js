ad560315a6e24a7c48e8329de05308f7c0a886a3
lion772 <williamlopes64@gmail.com>
2023-05-31 14:20:56 +0200

    output a dummy list to events page

--- new-router-app/frontend/src/pages/Events.js
+++ new-router-app/frontend/src/pages/Events.js
@@ -1,7 +1,24 @@
 import React from "react";
+import data from "../assets/data.json";
+import { Link } from "react-router-dom";
 
 const EventsPage = () => {
-    return <h1>EventsPage </h1>;
+    const DUMMY_EVENTS = data["dummy-events"];
+
+    return (
+        <>
+            <h1>EventsPage </h1>
+            <ul>
+                {DUMMY_EVENTS &&
+                    DUMMY_EVENTS.length > 0 &&
+                    DUMMY_EVENTS.map((event) => (
+                        <li key={event.id}>
+                            <Link to={event.id}>{event.title}</Link>
+                        </li>
+                    ))}
+            </ul>
+        </>
+    );
 };
 
 export default EventsPage;
