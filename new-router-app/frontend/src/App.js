9a56b3a21be44742ca4de62bb9e133396d39f89d
lion772 <williamlopes64@gmail.com>
2023-05-31 13:12:20 +0200

    create router definition in app js, add MainNavigation to Layout wrapping page

--- new-router-app/frontend/src/App.js
+++ new-router-app/frontend/src/App.js
@@ -3,17 +3,19 @@ import LayoutPage from "./pages/Layout";
 import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <LayoutPage />,
         children: [
-            { path: "/", element: <HomePage /> },
-            { path: "/events", element: <EventsPage /> },
-            { path: "/events/:eventId", element: <EventDetailPage /> },
-            { path: "/", element: HomePage },
-            { path: "/", element: HomePage },
+            { index: true, element: <HomePage /> },
+            { path: "events", element: <EventsPage /> },
+            { path: "events/:eventId", element: <EventDetailPage /> },
+            { path: "events/new", element: <NewEventPage /> },
+            { path: "events/:eventId/edit", element: <EditEventPage /> },
         ],
     },
 ]);
--- new-router-app/frontend/src/pages/Layout.js
+++ new-router-app/frontend/src/pages/Layout.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
 const LayoutPage = () => {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <>
+            <MainNavigation />
+            <main>
+                <Outlet />
+            </main>
+        </>
     );
 };
 
