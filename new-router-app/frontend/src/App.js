bd29b08c39562f6b248c2a93a02f120404d4decb
lion772 <williamlopes64@gmail.com>
2023-06-01 18:14:12 +0200

    submit data programmatically in EventDetail page

--- new-router-app/frontend/src/App.js
+++ new-router-app/frontend/src/App.js
@@ -4,8 +4,9 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
     loader as eventDetailLoader,
+    action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import EventsRootLayoutPage from "./pages/EventsRootLayout";
 import ErrorPage from "./pages/Error";
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
                             {
                                 index: true,
                                 element: <EventDetailPage />,
+                                action: deleteEventAction,
                             },
                             {
                                 path: "edit",
@@ -44,6 +46,7 @@ const router = createBrowserRouter([
                     {
                         path: "new",
                         element: <NewEventPage />,
+                        action: newEventAction,
                     },
                 ],
             },
--- new-router-app/frontend/src/pages/EventDetail.js
+++ new-router-app/frontend/src/pages/EventDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
@@ -10,7 +10,7 @@ const EventDetailPage = () => {
 
 export default EventDetailPage;
 
-export async function loader({ req, params }) {
+export async function loader({ request, params }) {
     const id = params.eventId;
     const response = await fetch(`http://localhost:8080/events/${id}`);
 
@@ -22,4 +22,21 @@ export async function loader({ req, params }) {
     }
 
     return response;
+}
+
+export async function action({ request, params }) {
+    const id = params.eventId;
+
+    const response = await fetch(`http://localhost:8080/events/${id}`, {
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        throw json(
+            { message: "Could not delete selected event." },
+            { status: 500 }
+        );
+    }
+
+    return redirect("..");
 }
