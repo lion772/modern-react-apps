a86ea69d61c241681aadc20dbe9d02eff055c229
lion772 <williamlopes64@gmail.com>
2023-06-15 23:16:19 +0200

    add createAszncThunk to fetch user list

---
+++ interface-async-thunks/src/store/thunks/fetchUsers.tsx
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchUsers = createAsyncThunk(
+    "users/fetch",
+    async (_, thunkAPI) => {
+        try {
+            // Perform an async operation here (e.g., API request)
+            const response = await axios.get("http://localhost:3005/users");
+            return await response.data;
+        } catch (error: any) {
+            // Handle errors
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
