f542abe4efbd7c9f80140f623271c100a292a968
lion772 <williamlopes64@gmail.com>
2023-06-18 19:26:16 +0200

    wrap up ExpandablePanel

--- interface-async-thunks/src/components/ExpandablePanel.tsx
+++ interface-async-thunks/src/components/ExpandablePanel.tsx
@@ -1,19 +1,28 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
 interface ExpandablePanelInt {
     header: ReactNode;
     children: ReactNode | undefined | null;
 }
 
-const ExpandablePanel: FC<ExpandablePanelInt> = ({ header, children }) => {
+const ExpandablePanel: FC<ExpandablePanelInt> = ({ header, children }): any => {
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
     return (
         <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex p-2 justify-between items-center">
                 <div className="flex flex-row items-center justify-between">
                     {header}
                 </div>
+                <div
+                    onClick={() => setIsExpanded(!isExpanded)}
+                    className="cursor-pointer"
+                >
+                    {!isExpanded ? <GoChevronLeft /> : <GoChevronDown />}
+                </div>
             </div>
-            <div className="p-2 border-t">{children}</div>
+            {isExpanded && <div className="p-2 border-t">{children}</div>}
         </div>
     );
 };
