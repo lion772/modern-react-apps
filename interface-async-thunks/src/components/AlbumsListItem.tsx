5bb91461a8cbca6bc0dc0f12bfa06f4438b93953
lion772 <williamlopes64@gmail.com>
2023-06-19 19:01:44 +0200

    generate AlbumsListItem

---
+++ interface-async-thunks/src/components/AlbumsListItem.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { Album } from "./Album";
+import ExpandablePanel from "./ExpandablePanel";
+import { GoTrashcan } from "react-icons/go";
+import Button from "./Button";
+import { useRemoveAlbumMutation } from "../store";
+
+interface AlbumsListItemInt {
+    album: Album;
+}
+
+const AlbumsListItem: FC<AlbumsListItemInt> = ({ album }) => {
+    const [removeAlbum, result] = useRemoveAlbumMutation();
+    console.log(result);
+    const header = (
+        <>
+            <Button
+                className="mr-2"
+                loading={result.isLoading}
+                onClick={() => removeAlbum(album)}
+            >
+                <GoTrashcan />
+            </Button>
+            {album.title}
+        </>
+    );
+    return (
+        <ExpandablePanel header={header}>
+            List of photos in the album
+        </ExpandablePanel>
+    );
+};
+
+export default AlbumsListItem;
