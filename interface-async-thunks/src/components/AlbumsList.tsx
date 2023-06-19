28cee5ab42bd7ee3d39894763a608369f4184f94
lion772 <williamlopes64@gmail.com>
2023-06-19 14:01:40 +0200

    add refetch with fine-grained tag validation

--- interface-async-thunks/src/components/AlbumsList.tsx
+++ interface-async-thunks/src/components/AlbumsList.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
 import { User } from "./User";
-import AlbunsListItem from "./AlbumsListItem";
-import { useFetchAlbumsQuery } from "../store";
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store";
 import Skeleton from "./Skeleton";
 import { Album } from "./Album";
 import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 interface AlbumsListInt {
     user: User;
@@ -12,14 +12,15 @@ interface AlbumsListInt {
 
 const AlbumsList: FC<AlbumsListInt> = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const [addAlbum, result] = useAddAlbumMutation();
 
-    let content = <></>;
+    let content: any;
     if (isLoading) {
         content = <Skeleton times={4} className="h-4 w-full" />;
     } else if (error) {
         content = <p>Couldn't fetch albums</p>;
     } else {
-        content = data.map((album: Album) => (
+        content = (data as Album[]).map((album: Album) => (
             <ExpandablePanel key={album.id} header={album.title}>
                 <div>List of photos in the album go here</div>
             </ExpandablePanel>
@@ -28,6 +29,9 @@ const AlbumsList: FC<AlbumsListInt> = ({ user }) => {
     return (
         <>
             <div>Albums for {user.name}</div>
+            <Button loading={isLoading} onClick={() => addAlbum(user)}>
+                Add Album
+            </Button>
             {content}
         </>
     );
--- interface-async-thunks/src/store/apis/albumsApi.tsx
+++ interface-async-thunks/src/store/apis/albumsApi.tsx
@@ -1,20 +1,35 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+import { Album } from "../../components/Album";
+import { User } from "../../components/User";
 
 const albumsApi = createApi({
     reducerPath: "albums",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3005" }),
-    endpoints: (builder) => ({
+    endpoints: (builder: any) => ({
         // Define your endpoints here
         fetchAlbums: builder.query({
-            query: (user) => ({
+            providesTags: (result: any, error: any, user: User) => [
+                { type: "Album", id: user.id },
+            ],
+            query: (user: { id: string }) => ({
                 url: "/albums",
                 params: { userId: user.id },
                 method: "GET",
             }),
         }),
-        // More endpoints...
+        addAlbum: builder.mutation({
+            invalidatesTags: (result: any, error: any, user: User) => [
+                { type: "Album", id: user.id },
+            ],
+            query: (user: { id: string }) => ({
+                url: "/albums",
+                method: "POST",
+                body: { userId: user.id, title: faker.commerce.productName() },
+            }),
+        }),
     }),
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
