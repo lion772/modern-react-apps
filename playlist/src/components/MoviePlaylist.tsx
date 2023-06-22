51dd09510fe43542b28ffe8226002d1da19b2fee
lion772 <williamlopes64@gmail.com>
2023-06-22 15:14:56 +0200

    wire up MoviePlaylist component

--- playlist/src/components/MoviePlaylist.tsx
+++ playlist/src/components/MoviePlaylist.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-
 function MoviePlaylist() {
-    return <div>MoviePlaylist</div>;
+    return (
+        <div className="content">
+            <div className="table-header">
+                <h3 className="subtitle is-3">Movie Playlist</h3>
+                <div className="buttons">
+                    <button
+                        className="button is-link"
+                    >
+                        + Add Movie to Playlist
+                    </button>
+                </div>
+            </div>
+            <ul></ul>
+        </div>
+    );
 }
 
 export default MoviePlaylist;
