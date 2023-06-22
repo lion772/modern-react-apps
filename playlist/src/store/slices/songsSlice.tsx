56e72aa8aa5e3c6a5a79b9f2d51c3225bb33c7ca
lion772 <williamlopes64@gmail.com>
2023-06-22 16:01:32 +0200

    add songsSlice, add addSong and removeSong reducers, export them

---
+++ playlist/src/store/slices/songsSlice.tsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+    name: "songs",
+    initialState: [],
+    reducers: {
+        addSong(state: string[], action: { payload: string }) {
+            state.push(action.payload);
+        },
+        removeSong(state: string[], action: { payload: string }) {
+            const indexFound = state.indexOf(action.payload);
+            //Remove 1 element at index found by indexOf
+            state.splice(indexFound, 1);
+        },
+    },
+});
+
+export const { addSong, removeSong } = songsSlice.actions;
+export const songsReducer = songsSlice.reducer;
